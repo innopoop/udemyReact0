c0574d0e19787f12b3c41155684c3826f2da8d4e
Ashley Zhu <ashleyrawr74@gmail.com>
2020-06-23 19:25:55 -0700

    Updates

---
+++ precache-manifest.3702eb7b49008caa733ee998c1c1bd7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "397af43f52f048b704c1eac571b89649",
+    "url": "/udemyReact0/index.html"
+  },
+  {
+    "revision": "f57c305978739060cffa",
+    "url": "/udemyReact0/static/css/main.ef2dc733.chunk.css"
+  },
+  {
+    "revision": "9a30ab37c14bf926dbc4",
+    "url": "/udemyReact0/static/js/2.af7781f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/udemyReact0/static/js/2.af7781f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f57c305978739060cffa",
+    "url": "/udemyReact0/static/js/main.af0a6c2f.chunk.js"
+  },
+  {
+    "revision": "30c99637d3db0b8c904d",
+    "url": "/udemyReact0/static/js/runtime-main.9e265d48.js"
+  }
+]);
